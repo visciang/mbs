6740e3ce9e1214d675deb0bf2abec73917e4ee11
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-03-06 10:28:54 +0100

    graph command check dot installed

--- mbs/lib/cli/command/graph.ex
+++ mbs/lib/cli/command/graph.ex
@@ -5,12 +5,24 @@ defimpl MBS.CLI.Command, for: MBS.CLI.Args.Graph do
   alias MBS.{Manifest, Workflow}
 
   def run(%Args.Graph{targets: target_ids, output_svg_file: output_svg_file}, %Config.Data{} = config, reporter) do
-    Manifest.find_all()
-    |> CLI.Utils.transitive_dependencies_closure(target_ids)
-    |> Workflow.workflow(config, reporter, fn _, _, _ -> :ok end)
-    |> Dask.Dot.export()
-    |> Dask.Utils.dot_to_svg(output_svg_file)
+    if dot_command_installed?() do
+      Manifest.find_all()
+      |> CLI.Utils.transitive_dependencies_closure(target_ids)
+      |> Workflow.workflow(config, reporter, fn _, _, _ -> :ok end)
+      |> Dask.Dot.export()
+      |> Dask.Utils.dot_to_svg(output_svg_file)
 
-    :ok
+      :ok
+    else
+      :error
+    end
+  end
+
+  defp dot_command_installed? do
+    System.cmd("dot", ["-V"])
+    true
+  rescue
+    ErlangError ->
+      false
   end
 end
