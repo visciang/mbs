2c22933fab04f94c7bc7de08d6de6e8827c2786f
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-03-08 19:22:16 +0100

    alias

--- ci.sh
+++ ci.sh
@@ -5,14 +5,6 @@ set -e
 docker build --rm -f dockerfiles/Dockerfile --target slim --tag mbs:slim .
 docker build --rm -f dockerfiles/Dockerfile --target full --tag mbs:full .
 
-alias mbs="\
-    docker run --init --rm -ti \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $PWD:$PWD \
-    -w $PWD \
-    -e MBS_ROOT=$PWD \
-    mbs:slim"
-
-mbs version
-mbs ls --verbose
-mbs run --logs
+./mbs.sh version
+./mbs.sh ls --verbose
+./mbs.sh run --logs
