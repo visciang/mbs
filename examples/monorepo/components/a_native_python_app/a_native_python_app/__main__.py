4870baff80a09529de07f9a82e3d3feb8063a6b6
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-03-08 08:14:49 +0100

    rename examples

--- examples/monorepo/components/a_native_python_app/a_native_python_app/__main__.py
+++
@@ -1,6 +0,0 @@
-import a_python_library
-
-print("APP running")
-
-print("calling a_python_library.hello()")
-a_python_library.hello()
---
+++ examples/monorepo/components/python_native_app/python_native_app/__main__.py
@@ -0,0 +1,6 @@
+import python_library
+
+print("APP running")
+
+print("calling python_library.hello()")
+python_library.hello()
