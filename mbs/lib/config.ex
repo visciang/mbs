2fd453b767194b008f823e2e2af92267cd68f1b9
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-03-09 13:54:22 +0100

    validate .mbs-config

--- mbs/lib/config.ex
+++ mbs/lib/config.ex
@@ -21,36 +21,51 @@ defmodule MBS.Config do
   @config_file ".mbs-config.json"
 
   def load do
-    case File.read(@config_file) do
+    @config_file
+    |> File.read()
+    |> case do
       {:ok, conf_data} ->
-        parse(conf_data)
+        conf_data
 
       {:error, reason} ->
         Utils.halt("Cannot read configuration file #{@config_file} (#{reason})")
     end
+    |> decode()
+    |> add_defaults()
+    |> validate()
+    |> to_struct()
   end
 
-  defp parse(conf_data) do
-    case Jason.decode(conf_data) do
+  defp decode(conf_data) do
+    conf_data
+    |> Jason.decode()
+    |> case do
       {:ok, conf} ->
-        conf = to_struct(conf)
-        put_in(conf.root_dir, mbs_root_dir())
+        conf
 
       {:error, reason} ->
         Utils.halt("Error parsing configuration file #{@config_file}\n  #{Jason.DecodeError.message(reason)}")
     end
   end
 
-  defp to_struct(conf) do
-    default_parallelism = :erlang.system_info(:logical_processors)
+  defp add_defaults(conf) do
+    conf = put_in(conf["root_dir"], mbs_root_dir())
 
-    %Data{
-      parallelism: conf |> Map.get("parallelism", default_parallelism),
-      cache: %Data.Cache{
-        dir: conf |> Map.fetch!("cache") |> Map.fetch!("dir") |> Path.expand()
-      },
-      timeout: conf |> Map.get("timeout", :infinity)
-    }
+    conf =
+      if conf["parallelism"] == nil do
+        put_in(conf["parallelism"], :erlang.system_info(:logical_processors))
+      else
+        conf
+      end
+
+    conf =
+      if conf["timeout"] == nil do
+        put_in(conf["timeout"], :infinity)
+      else
+        conf
+      end
+
+    conf
   end
 
   defp mbs_root_dir do
@@ -61,5 +76,48 @@ defmodule MBS.Config do
       root_dir ->
         root_dir
     end
+  end
+
+  defp validate(conf) do
+    validate_root_dir(conf["root_dir"])
+    validate_parallelism(conf["parallelism"])
+    validate_cache(conf["cache"])
+    validate_timeout(conf["timeout"])
+    conf
+  end
+
+  def validate_root_dir(root_dir) do
+    unless is_binary(root_dir) and File.exists?(root_dir) do
+      Utils.halt("Bad root_dir in #{@config_file}")
+    end
+  end
+
+  def validate_parallelism(parallelism) do
+    unless is_integer(parallelism) and parallelism > 0 do
+      Utils.halt("Bad parallelism in #{@config_file}")
+    end
+  end
+
+  def validate_cache(cache) do
+    unless is_binary(cache["dir"]) and Path.type(cache["dir"]) == :relative do
+      Utils.halt("Bad cache dir in #{@config_file}. Should be a relative path (in the repo root)")
+    end
+  end
+
+  def validate_timeout(timeout) do
+    unless timeout == :infinity or (is_integer(timeout) and timeout > 0) do
+      Utils.halt("Bad timeout in #{@config_file}")
+    end
+  end
+
+  defp to_struct(conf) do
+    %Data{
+      root_dir: conf["root_dir"],
+      parallelism: conf["parallelism"],
+      cache: %Data.Cache{
+        dir: conf["cache"]["dir"] |> Path.expand()
+      },
+      timeout: conf["timeout"]
+    }
   end
 end
