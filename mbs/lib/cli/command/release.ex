03109c364a24466a0d8ea65d40f41cd91fa16619
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-03-16 19:07:21 +0100

    release manifest

--- mbs/lib/cli/command/release.ex
+++ mbs/lib/cli/command/release.ex
@@ -11,13 +11,18 @@ end
 
 defimpl MBS.CLI.Command, for: MBS.CLI.Command.Release do
   alias MBS.CLI.{Command, Reporter}
-  alias MBS.{CLI, Config, Manifest, Utils, Workflow}
+  alias MBS.{Checksum, CLI, Config, Manifest, Utils, Workflow}
 
   @spec run(Command.Release.t(), Config.Data.t(), Reporter.t()) :: Dask.await_result()
-  def run(%Command.Release{targets: target_ids, output_dir: output_dir}, %Config.Data{} = config, reporter) do
-    dask =
+  def run(%Command.Release{tag: tag, targets: target_ids, output_dir: output_dir}, %Config.Data{} = config, reporter) do
+    File.mkdir_p!(output_dir)
+
+    manifests =
       Manifest.find_all()
       |> CLI.Utils.transitive_dependencies_closure(target_ids)
+
+    dask =
+      manifests
       |> Workflow.workflow(
         config,
         reporter,
@@ -33,6 +38,35 @@ defimpl MBS.CLI.Command, for: MBS.CLI.Command.Release do
           Utils.halt(error.message)
       end
 
-    Dask.await(dask)
+    res = Dask.await(dask)
+
+    release_manifest(manifests, tag, output_dir)
+
+    res
+  end
+
+  defp release_manifest(manifests, tag, output_dir) do
+    {commit, 0} = System.cmd("git", ["rev-parse", "HEAD"])
+
+    release_manifest = %{
+      tag: tag,
+      commit: commit |> String.trim(),
+      checksum: release_checksum(manifests, output_dir)
+    }
+
+    release_manifest_path = Path.join(output_dir, "manifest.json")
+    File.write!(release_manifest_path, Jason.encode!(release_manifest, pretty: true))
+  end
+
+  defp release_checksum(manifests, output_dir) do
+    manifests
+    |> Enum.map(fn %{id: id} ->
+      Path.join([output_dir, id, "manifest.json"])
+      |> File.read!()
+      |> Jason.decode!()
+      |> Map.fetch!("checksum")
+    end)
+    |> Enum.join()
+    |> Checksum.checksum("")
   end
 end
