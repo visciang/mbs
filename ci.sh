be4958e03dc3267dbe885218aaca95ccbbe0e0bf
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-03-26 05:48:19 +0100

    more run_it

--- ci.sh
+++ ci.sh
@@ -7,12 +7,14 @@ docker build --rm -f dockerfiles/Dockerfile --target full --tag mbs:full .
 
 if [ ! -z "$RUN_IT" ]; then
     ./mbs.sh version
+    ./mbs.sh --help
     ./mbs.sh build ls
     ./mbs.sh build ls --verbose
     ./mbs.sh deploy ls
     ./mbs.sh deploy ls --verbose
+    ./mbs.sh deploy tree
     ./mbs.sh build outdated
-    ./mbs.sh build tree mbs
+    ./mbs.sh build tree
     ./mbs.sh build run --logs
     ./mbs.sh build graph
 fi
