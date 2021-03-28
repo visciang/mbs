a36260567896e51d9388566585d5bf8825d28cae
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-03-28 14:40:19 +0200

    optimize log filtering client side (use protected ETS)

--- mbs/lib/cli/reporter.ex
+++ mbs/lib/cli/reporter.ex
@@ -16,14 +16,12 @@ defmodule MBS.CLI.Reporter do
   defmodule State do
     @moduledoc false
 
-    defstruct [:start_time, :success_jobs, :failed_jobs, :muted, :logs_enabled]
+    defstruct [:start_time, :success_jobs, :failed_jobs]
 
     @type t :: %__MODULE__{
             start_time: integer(),
             failed_jobs: [String.t()],
-            success_jobs: [String.t()],
-            muted: boolean(),
-            logs_enabled: boolean()
+            success_jobs: [String.t()]
           }
   end
 
@@ -50,7 +48,18 @@ defmodule MBS.CLI.Reporter do
 
   @spec job_report(String.t(), Status.t(), nil | String.t(), nil | non_neg_integer()) :: :ok
   def job_report(job_id, status, description, elapsed) do
-    GenServer.call(@name, %Report{job_id: job_id, status: status, description: description, elapsed: elapsed})
+    report? =
+      cond do
+        :ets.lookup_element(@name, :muted, 2) -> false
+        status == Status.log() and not :ets.lookup_element(@name, :logs_enabled, 2) -> false
+        true -> true
+      end
+
+    if report? do
+      GenServer.call(@name, %Report{job_id: job_id, status: status, description: description, elapsed: elapsed})
+    else
+      :ok
+    end
   end
 
   @spec time :: integer()
@@ -60,12 +69,10 @@ defmodule MBS.CLI.Reporter do
 
   @impl true
   def init(start_time: start_time) do
-    {:ok, %State{start_time: start_time, muted: false, logs_enabled: false, success_jobs: [], failed_jobs: []}}
-  end
+    :ets.new(@name, [:named_table])
+    :ets.insert(@name, [{:muted, false}, {:logs_enabled, false}])
 
-  @impl true
-  def handle_cast(%Report{status: Status.log()}, %State{logs_enabled: false} = state) do
-    {:noreply, state}
+    {:ok, %State{start_time: start_time, success_jobs: [], failed_jobs: []}}
   end
 
   @impl true
@@ -86,9 +93,9 @@ defmodule MBS.CLI.Reporter do
         job_id
       end
 
-    puts(IO.ANSI.format([status_icon, " - ", :bright, job_id, :normal, "  ", duration, " ", description]), state)
+    puts(IO.ANSI.format([status_icon, " - ", :bright, job_id, :normal, "  ", duration, " ", description]))
 
-    if status_info, do: IO.puts("  - #{status_info}")
+    if status_info, do: puts("  - #{status_info}")
 
     state = track_jobs(job_id, status, state)
 
@@ -97,12 +104,14 @@ defmodule MBS.CLI.Reporter do
 
   @impl true
   def handle_call({:mute, status}, _from, %State{} = state) do
-    {:reply, :ok, put_in(state.muted, status)}
+    :ets.insert(@name, {:muted, status})
+    {:reply, :ok, state}
   end
 
   @impl true
   def handle_call({:logs, enabled}, _from, %State{} = state) do
-    {:reply, :ok, put_in(state.logs_enabled, enabled)}
+    :ets.insert(@name, {:logs_enabled, enabled})
+    {:reply, :ok, state}
   end
 
   @impl true
@@ -120,7 +129,7 @@ defmodule MBS.CLI.Reporter do
 
     duration = delta_time_string(end_time - state.start_time)
 
-    puts("\n#{log_message} (#{duration})", state)
+    puts("\n#{log_message} (#{duration})")
 
     {:stop, :normal, :ok, state}
   end
@@ -149,6 +158,9 @@ defmodule MBS.CLI.Reporter do
     end
   end
 
-  defp puts(_message, %State{muted: true}), do: :ok
-  defp puts(message, %State{muted: false}), do: IO.puts(message)
+  defp puts(message) do
+    unless :ets.lookup_element(@name, :muted, 2) do
+      IO.puts(message)
+    end
+  end
 end
