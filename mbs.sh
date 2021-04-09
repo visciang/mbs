c18110d62a7555b70079295c358a0c0ecf287a7a
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-04-09 19:38:42 +0200

    tty

--- mbs.sh
+++ mbs.sh
@@ -12,8 +12,12 @@ MBS_CACHE_VOLUME="mbs-cache"
 MBS_RELEASE_VOLUME="mbs-releases"
 MBS_GRAPH_VOLUME="$ABS_BASEDIR/.mbs-graph"
 
+
+TTY="-ti"
+if [ ! -t 1 ]; then TTY=""; fi
+
 alias mbs="\
-    docker run --init --rm -ti \
+    docker run --init --rm $TTY \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $MBS_CACHE_VOLUME:/.mbs-cache \
     -v $MBS_RELEASE_VOLUME:/.mbs-releases \
