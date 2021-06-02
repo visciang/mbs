5f0ff939cf7527a8ceb6fab871eaf3248cfa10fc
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-06-02 17:39:59 +0200

    validation

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -7,15 +7,21 @@ CMD=$1
 SUBCMD=$2
 
 if [ "$CMD" == "bootstrap" ]; then
-    MBS_PROJECT_ID=$(jq -r '.project' .mbs-config.json) \
-    MBS_PUSH=$(jq -r '.remote_cache.push' .mbs-config.json) \
-    MBS_REMOTE_CACHE_VOLUME=$(jq -r '.remote_cache.volume' .mbs-config.json) \
-    envsubst '
-        ${MBS_PROJECT_ID}
-        ${MBS_PUSH}
-        ${MBS_REMOTE_CACHE_VOLUME}' \
-        < /bootstrap.template.sh
-elif [ "$CMD" == "build" ] && [ "$SUBCMD" == "shell" ]; then
+    MBS_PROJECT_ID=$(jq -r '.project' .mbs-config.json)
+    MBS_PUSH=$(jq -r '.remote_cache.push' .mbs-config.json)
+    MBS_REMOTE_CACHE_VOLUME=$(jq -r '.remote_cache.volume' .mbs-config.json)
+    MBS_REMOTE_CACHE_DOCKER_REGISTRY=$(jq -r '.remote_cache.docker_registry' .mbs-config.json)
+
+    if [ "$MBS_PUSH" = "true" ]; then
+        if [ "$MBS_REMOTE_CACHE_VOLUME" = "null" -o "$MBS_REMOTE_CACHE_DOCKER_REGISTRY" = "null" ]; then
+            >&2 echo "Bad config: .remote_cache.push='true' implies a non null .remote_cache.volume / .remote_cache.docker_registry"
+            exit 1
+        fi
+    fi
+
+    MBS_PROJECT_ID=$MBS_PROJECT_ID MBS_PUSH=$MBS_PUSH MBS_REMOTE_CACHE_VOLUME=$MBS_REMOTE_CACHE_VOLUME \
+    envsubst '${MBS_PROJECT_ID} ${MBS_PUSH} ${MBS_REMOTE_CACHE_VOLUME}' < /bootstrap.template.sh
+elif [ "$CMD" == "build" -a "$SUBCMD" == "shell" ]; then
     $MBS $@
 
     SHELL_TARGET=$3
