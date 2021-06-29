3707966a6bed277ba6b95d0d84631f8705a8cfd2
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-06-29 07:17:52 +0200

    logs to file

--- src/mbs/lib/cli/reporter.ex
+++ src/mbs/lib/cli/reporter.ex
@@ -4,6 +4,7 @@ defmodule MBS.CLI.Reporter do
   use GenServer
 
   alias MBS.CLI.Reporter.{Report, Status}
+  alias MBS.Const
 
   require MBS.CLI.Reporter.Status
 
@@ -15,9 +16,11 @@ defmodule MBS.CLI.Reporter do
   defmodule State do
     @moduledoc false
 
-    defstruct [:start_time, :success_jobs, :failed_jobs]
+    defstruct [:logs_dir, :job_id_to_log_file, :start_time, :success_jobs, :failed_jobs]
 
     @type t :: %__MODULE__{
+            logs_dir: Path.t(),
+            job_id_to_log_file: %{String.t() => File.io_device()},
             start_time: integer(),
             failed_jobs: [String.t()],
             success_jobs: [String.t()]
@@ -72,7 +75,17 @@ defmodule MBS.CLI.Reporter do
     :ets.new(@name, [:named_table])
     :ets.insert(@name, [{:muted, false}, {:logs_enabled, false}])
 
-    {:ok, %State{start_time: start_time, success_jobs: [], failed_jobs: []}}
+    logs_dir = Path.join(Const.logs_dir(), to_string(DateTime.utc_now()))
+    File.mkdir_p!(logs_dir)
+
+    {:ok,
+     %State{
+       logs_dir: logs_dir,
+       job_id_to_log_file: %{},
+       start_time: start_time,
+       success_jobs: [],
+       failed_jobs: []
+     }}
   end
 
   @impl true
@@ -81,23 +94,21 @@ defmodule MBS.CLI.Reporter do
         _from,
         %State{} = state
       ) do
-    {status_icon, status_info} = status_icon_info(status)
+    {state, log_file} = get_log_file(state, job_id)
+    state = track_jobs(job_id, status, state)
 
     duration = if elapsed != nil, do: " (#{delta_time_string(elapsed)}) ", else: ""
     description = if description != nil, do: "| #{description}", else: ""
+    {status_icon, status_info} = status_icon_info(status)
 
-    job_id =
-      if status == Status.log() do
-        IO.ANSI.format([:yellow, job_id])
-      else
-        job_id
-      end
+    job_id = if status == Status.log(), do: IO.ANSI.format([:yellow, job_id]), else: job_id
 
-    puts(IO.ANSI.format([status_icon, " - ", :bright, job_id, :normal, "  ", duration, " ", :faint, description]))
+    puts_log(
+      log_file,
+      IO.ANSI.format([status_icon, " - ", :bright, job_id, :normal, "  ", duration, " ", :faint, description])
+    )
 
-    if status_info, do: puts("  - #{status_info}")
-
-    state = track_jobs(job_id, status, state)
+    if status_info, do: puts_log(log_file, "  - #{status_info}")
 
     {:reply, :ok, state}
   end
@@ -115,14 +126,16 @@ defmodule MBS.CLI.Reporter do
   end
 
   @impl true
-  def handle_call({:stop, reason}, _from, %State{success_jobs: success_jobs, failed_jobs: failed_jobs} = state) do
+  def handle_call(
+        {:stop, reason},
+        _from,
+        %State{logs_dir: logs_dir, job_id_to_log_file: job_id_to_log_file, success_jobs: success_jobs, failed_jobs: failed_jobs} = state
+      ) do
     end_time = time()
-
-    success_jobs_count = length(success_jobs)
 
     log_message =
       case reason do
-        :ok -> IO.ANSI.format([:green, "Completed (#{success_jobs_count} jobs)"])
+        :ok -> IO.ANSI.format([:green, "Completed (#{length(success_jobs)} jobs)"])
         :error -> IO.ANSI.format([:red, "Failed jobs:", :normal, Enum.map(Enum.sort(failed_jobs), &"\n- #{&1}"), "\n"])
         :timeout -> IO.ANSI.format([:red, "Timeout"])
       end
@@ -130,16 +143,36 @@ defmodule MBS.CLI.Reporter do
     duration = delta_time_string(end_time - state.start_time)
 
     puts("\n#{log_message} (#{duration})")
+    puts("\nLogs directory: #{logs_dir}")
+
+    job_id_to_log_file
+    |> Map.values()
+    |> Enum.each(&File.close/1)
 
     {:stop, :normal, :ok, state}
+  end
+
+  @spec get_log_file(State.t(), String.t()) :: {State.t(), File.io_device()}
+
+  defp get_log_file(
+         %State{logs_dir: logs_dir, job_id_to_log_file: job_id_to_log_file} = state,
+         job_id
+       ) do
+    if Map.has_key?(job_id_to_log_file, job_id) do
+      {state, job_id_to_log_file[job_id]}
+    else
+      file = File.open!(Path.join(logs_dir, "#{job_id}.txt"), [:utf8, :write])
+      state = update_in(state.job_id_to_log_file, &Map.put(&1, job_id, file))
+      {state, file}
+    end
   end
 
   @spec track_jobs(String.t(), Status.t(), State.t()) :: State.t()
   defp track_jobs(job_id, status, state) do
     case status do
-      Status.error(_reason) -> put_in(state.failed_jobs, [job_id | state.failed_jobs])
-      Status.ok() -> put_in(state.success_jobs, [job_id | state.success_jobs])
-      Status.uptodate() -> put_in(state.success_jobs, [job_id | state.success_jobs])
+      Status.error(_reason) -> update_in(state.failed_jobs, &[job_id | &1])
+      Status.ok() -> update_in(state.success_jobs, &[job_id | &1])
+      Status.uptodate() -> update_in(state.success_jobs, &[job_id | &1])
       _ -> state
     end
   end
@@ -161,12 +194,19 @@ defmodule MBS.CLI.Reporter do
     end
   end
 
+  @spec puts_log(nil | File.io_device(), IO.chardata()) :: :ok
+  defp puts_log(log_file, message) do
+    puts(message)
+
+    if log_file != nil do
+      IO.write(log_file, message)
+      IO.write(log_file, "\n")
+    end
+  end
+
   @spec puts(IO.chardata()) :: :ok
   defp puts(message) do
-    unless :ets.lookup_element(@name, :muted, 2) do
-      IO.puts(message)
-    end
-
+    unless :ets.lookup_element(@name, :muted, 2), do: IO.puts(message)
     :ok
   end
 end
