cd7aefbad90428facdacf9e67c73a1e7e80b9497
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-12-15 20:43:18 +0100

    hot fix!

--- src/mbs/lib/workflow/job/run_build/context/files.ex
+++ src/mbs/lib/workflow/job/run_build/context/files.ex
@@ -11,18 +11,21 @@ defmodule MBS.Workflow.Job.RunBuild.Context.Files do
   def put(%BuildDeploy.Component{id: id} = component, upstream_components, true) do
     temp_dir = Utils.mktemp()
 
-    [component | upstream_components]
-    |> Enum.each(fn %BuildDeploy.Component{files: files} ->
-      files
-      |> paths_dirname()
-      |> Enum.each(&File.mkdir_p!(Path.join(temp_dir, &1)))
+    files =
+      [component | upstream_components]
+      |> Enum.flat_map(fn %BuildDeploy.Component{files: files} -> files end)
+      |> MapSet.new()
 
-      Enum.each(files, &File.ln_s!(&1, Path.join(temp_dir, &1)))
-    end)
+    files
+    |> paths_dirname()
+    |> Enum.each(&File.mkdir_p!(Path.join(temp_dir, &1)))
+
+    files
+    |> Enum.each(&File.ln_s!(&1, Path.join(temp_dir, &1)))
 
     Docker.container_dput(id, temp_dir, "/", id)
   end
 
-  @spec paths_dirname([Path.t()]) :: MapSet.t(Path.t())
+  @spec paths_dirname(MapSet.t(Path.t())) :: MapSet.t(Path.t())
   defp paths_dirname(paths), do: MapSet.new(paths, &Path.dirname(&1))
 end
